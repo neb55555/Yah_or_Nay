79be5ee49001da407d3bd435b75f1dc49ad08d8a
neb55555 <wiirterrific@gmail.com>
2016-06-06 10:54:29 -0400

    sdcxsdvgbrhcnj

---
+++ Assets/Script/move.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent <Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Vector2 move = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		rb.MovePosition (rb.position+move*speed*Time.deltaTime);
+	}
+}
